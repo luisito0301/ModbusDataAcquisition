150501fd363a07a2c62d0bc26279ac5700284aed
luisito0301 <luuffy90@gmail.com>
2024-12-08 15:20:30 +0100

    PRUEBA2E

--- ModbusData.DataAccess/FluentConfigurations/Common/EntityTypeConfigurationBase.cs
+++ ModbusData.DataAccess/FluentConfigurations/Common/EntityTypeConfigurationBase.cs
@@ -16,7 +16,7 @@ namespace ModbusData.DataAccess.FluentConfigurations.Common
     {
         public virtual void Configure(EntityTypeBuilder<T> builder)
         {
-            //MOC
+            //MOCKJHKJH
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).IsRequired();
         }
