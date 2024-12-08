22ad5031916b7f6fd6d6c0c322a986ade773c1ee
luisito0301 <luuffy90@gmail.com>
2024-12-08 15:17:07 +0100

    aDRIAN

---
+++ ModbusData.DataAccess/FluentConfigurations/ModbusNetworks/ModbusNetworkEntityTypeConfigurationBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ModbusData.DataAccess.FluentConfigurations.Common;
+using ModbusData.Domain.Entities.Device;
+using ModbusData.Domain.Entities.Modbus_Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModbusData.DataAccess.FluentConfigurations.ModbusNetworks
+{
+    public class ModbusNetworkEntityTypeConfigurationBase : EntityTypeConfigurationBase<ModbusNetwork>
+    {
+        public override void Configure(EntityTypeBuilder<ModbusNetwork> builder)
+        {
+            //NICE
+            builder.ToTable("ModbusNetwork");
+            base.Configure(builder);
+            builder.HasMany(x => x.Slaves)
+                .WithOne().HasForeignKey(x => x.Id);
+        }
+    }
+}
+
