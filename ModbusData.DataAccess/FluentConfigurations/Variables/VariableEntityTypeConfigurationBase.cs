fde3ad34e6b855db3e526bc4aa88b1651ee856e3
luisito0301 <luuffy90@gmail.com>
2024-12-08 15:12:09 +0100

    FluentConfigurations

---
+++ ModbusData.DataAccess/FluentConfigurations/Variables/VariableEntityTypeConfigurationBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ModbusData.DataAccess.FluentConfigurations.Common;
+using ModbusData.Domain.Entities.Variables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModbusData.DataAccess.FluentConfigurations.Variables
+{
+    public class VariableEntityTypeConfigurationBase : EntityTypeConfigurationBase<Variable>
+    {
+        public override void Configure(EntityTypeBuilder<Variable> builder)
+        {
+            builder.ToTable("Variable");
+            base.Configure(builder);
+            
+        }
+    }
+}
