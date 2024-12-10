709abffe4d1792b9a35493230727b074ecd36d7c
luisito0301 <luuffy90@gmail.com>
2024-12-09 22:05:22 -0500

    Repo_Dpne_not_reviewed
    Added Repos with bases and interfaces

---
+++ ModbusData.DataAccess/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿
+using ModbusData.DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using ModbusData.Contract;
+
+namespace ModbusData.DataAccess
+{
+    /// <summary>
+    /// Implementación de <see cref="IUnitOfWork"/>.
+    /// </summary>
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext _context;
+
+        public UnitOfWork(ApplicationContext context)
+        {
+            _context = context;
+            if (!context.Database.CanConnect())
+                context.Database.Migrate();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ ModusData.Contracts/Variables/IVariableRepository.cs
@@ -0,0 +1,19 @@
+﻿using ModbusData.Domain.Entities.Variables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModbusData.Contract.Variables
+{
+    public interface IVariableRepository
+    {
+        void AddVariable(Variable variable);
+        void DeleteVariable(Variable variable);
+        IEnumerable<T> GetAllVariables<T>() where T : Variable;
+        T? GetVariableById<T>(int id) where T : Variable;
+        void UpdateVariable(Variable variable);
+    }
+
+}
