ee9762192ea58f0328e29dfd3cf8a5979a4eb209
luisito0301 <141586540+luisito0301@users.noreply.github.com>
2025-01-06 20:11:06 +0100

    Update Program.cs

--- ModbusData.Services/Program.cs
+++ ModbusData.Services/Program.cs
@@ -1,9 +1,10 @@
+using ModbusData.Application;
 using ModbusData.Contract;
 using ModbusData.Contract.Variables;
 using ModbusData.DataAccess;
 using ModbusData.DataAccess.Contexts;
 using ModbusData.DataAccess.Repositories.Variables;
-using System.Reflection.Metadata;
+
 using ModbusData.GrpcProtos;
 using ModbusData.Services.Services;
 
@@ -14,6 +15,13 @@ namespace ModbusData.Services
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            
+            // Add services to the container.
+            builder.Services.AddSingleton("Data Source=ModbusDataDB.sqlite");
+            builder.Services.AddScoped<ApplicationContext>();
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped(typeof(IVariableRepository<>), typeof(VariableRepository<>));
+
             builder.Services.AddGrpc();
             builder.Services.AddAutoMapper(typeof(Program).Assembly);
             builder.Services.AddMediatR(new MediatRServiceConfiguration()
@@ -21,15 +29,8 @@ namespace ModbusData.Services
                 AutoRegisterRequestProcessors = true,
             }
              .RegisterServicesFromAssemblies(typeof(AssemblyReference).Assembly));
-            // Add services to the container.
-            builder.Services.AddSingleton("Data Source=ModbusDataDB.sqlite");
-            builder.Services.AddScoped<ApplicationContext>();
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-            builder.Services.AddScoped(typeof(IVariableRepository<>), typeof(VariableRepository<>));
 
-           
-           
-           
+
             builder.Logging.AddConsole(); // Esto permite que los logs se muestren en la consola
             builder.Logging.AddDebug(); // Esto permite que los logs se muestren en la ventana de salida de Visual Studio
 
