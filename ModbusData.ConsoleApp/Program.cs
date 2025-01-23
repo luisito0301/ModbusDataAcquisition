4265edd02ea30b9f578563196ab569015054785c
luisito0301 <luuffy90@gmail.com>
2025-01-22 20:52:06 -0500

    done

--- ModbusData.ConsoleApp/Program.cs
+++ ModbusData.ConsoleApp/Program.cs
@@ -1,7 +1,6 @@
-﻿using ModbusData.GrpcProtos; // Ensure you have the correct namespace for your gRPC Protos
+﻿using ModbusData.GrpcProtos; // Asegúrate de tener el namespace correcto para tus gRPC Protos
 using Grpc.Net.Client;
 using System;
-using System.Diagnostics.Metrics;
 using Google.Protobuf.WellKnownTypes;
 
 namespace ModbusData.ConsoleApp
@@ -33,6 +32,8 @@ namespace ModbusData.ConsoleApp
             var unitClient = new ModbusData.GrpcProtos.Unit.UnitClient(channel);
             var variableClient = new ModbusData.GrpcProtos.AnalogicVariable.AnalogicVariableClient(channel);
             var variableClient2 = new ModbusData.GrpcProtos.DigitalVariable.DigitalVariableClient(channel);
+            var slaveDeviceClient = new ModbusData.GrpcProtos.SlaveDeviceService.SlaveDeviceServiceClient(channel);
+            var modbusNetworkClient = new ModbusData.GrpcProtos.ModbusNetworkService.ModbusNetworkServiceClient(channel);
 
             // Crear una unidad
             Console.WriteLine("Presione una tecla para crear una unidad");
@@ -46,7 +47,7 @@ namespace ModbusData.ConsoleApp
                 Variables = { } // Puedes dejarlo vacío o agregar variables si es necesario
             });
 
-            if (createUnitResponse is null)
+            if (createUnitResponse == null)
             {
                 Console.WriteLine("Cannot create unit");
                 channel.Dispose();
@@ -64,15 +65,15 @@ namespace ModbusData.ConsoleApp
             var createResponse = variableClient.CreateAnalogicVariable(new CreateAnalogicVariableRequest()
             {
                 Name = "Test Variable",
-                Type = VariableType.Analogic, // Adjust as necessary
+                Type = VariableType.Analogic, // Ajustar según sea necesario
                 IsMeasurement = true,
                 Code = "TV001",
-                SamplingPeriod = "00:00:01", // Example TimeSpan
+                SamplingPeriod = "00:00:01", // Ejemplo de TimeSpan
                 ModbusAddress = 123,
                 Unitid = createUnitResponse.Id // Asignar el ID de la unidad creada
             });
 
-            if (createResponse is null)
+            if (createResponse == null)
             {
                 Console.WriteLine("Cannot create analogic variable");
                 channel.Dispose();
@@ -82,6 +83,7 @@ namespace ModbusData.ConsoleApp
             {
                 Console.WriteLine($"Creación exitosa de la variable analógica. ID: {createResponse.Id}");
             }
+
             // Crear una variable digital y asignar el ID de la unidad
             Console.WriteLine("Presione una tecla para crear una variable digital");
             Console.ReadKey();
@@ -89,16 +91,15 @@ namespace ModbusData.ConsoleApp
             var createResponse2 = variableClient2.CreateDigitalVariable(new CreateDigitalVariableRequest()
             {
                 Name = "Test Variable",
-                Type = VariableType.Digital, // Adjust as necessary
+                Type = VariableType.Digital, // Ajustar según sea necesario
                 IsMeasurement = true,
                 Code = "TV002",
-                SamplingPeriod = "00:00:01", // Example TimeSpan
+                SamplingPeriod = "00:00:01", // Ejemplo de TimeSpan
                 ModbusAddress = 123,
-                
                 Unitid = createUnitResponse.Id // Asignar el ID de la unidad creada
             });
 
-            if (createResponse2 is null)
+            if (createResponse2 == null)
             {
                 Console.WriteLine("Cannot create digital variable");
                 channel.Dispose();
@@ -109,10 +110,54 @@ namespace ModbusData.ConsoleApp
                 Console.WriteLine($"Creación exitosa de la variable digital. ID: {createResponse2.Id}");
             }
 
-            // Resto del código para obtener, modificar y eliminar la variable analógica...
-            // (El código que ya tenías para obtener, modificar y eliminar la variable analógica)
+            // Crear un dispositivo esclavo
+            Console.WriteLine("Presione una tecla para crear un dispositivo esclavo");
+            Console.ReadKey();
+
+            var createSlaveDeviceRequest = new CreateSlaveDeviceRequest()
+            {
+                
+                IpAddress = "192.168.1.2" // Ejemplo de IP
+            };
+            createSlaveDeviceRequest.Variables.Add(new Variable { }); // Agregar variable analógica
+            createSlaveDeviceRequest.Variables.Add(new Variable { }); // Agregar variable digital
+
+            var createSlaveDeviceResponse = slaveDeviceClient.CreateSlaveDevice(createSlaveDeviceRequest);
+
+            if (createSlaveDeviceResponse == null)
+            {
+                Console.WriteLine("Cannot create slave device");
+                channel.Dispose();
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"Creación exitosa del dispositivo esclavo. ID: {createSlaveDeviceResponse.Id}");
+            }
+
+            // Crear una red Modbus
+            Console.WriteLine("Presione una tecla para crear una red Modbus");
+            Console.ReadKey();
+
+            var createModbusNetworkResponse = modbusNetworkClient.CreateModbusNetwork(new CreateModbusNetworkRequest()
+            {
+                MasterIpAddress = "192.168.1.100", // Ejemplo de IP
+                Slaves = { createSlaveDeviceResponse.Id } // Ejemplo de dispositivos esclavos
+            });
+
+            if (createModbusNetworkResponse == null)
+            {
+                Console.WriteLine("Cannot create Modbus network");
+                channel.Dispose();
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"Creación exitosa de la red Modbus. ID: {createModbusNetworkResponse.Id}");
+            }
 
             channel.Dispose();
+            Console.WriteLine("Pruebas de integración finalizadas.");
         }
     }
 }
